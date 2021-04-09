0201f53ccb67914f0776567569afedea4ff8b871
dendup27 <dendup27@gmail.com>
2021-04-09 11:12:11 +0600

    Testing the test case

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -228,7 +228,8 @@ namespace CommandAPI.Tests
             var result = controller.DeleteCommand(0);
 
             //Assert
-            Assert.IsType<NotFoundResult>(result);
+            //Assert.IsType<NotFoundResult>(result);
+            Assert.IsType<OkResult>(result);
         }
 
         private List<Command> GetCommands(int num)
